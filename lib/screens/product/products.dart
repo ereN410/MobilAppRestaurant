d14eb089f2d5256b8aa24c9a591b505562e8bd60
ereN410 <72979148+ereN410@users.noreply.github.com>
2024-04-27 05:36:06 +0300

    Update products.dart

--- lib/screens/product/products.dart
+++ lib/screens/product/products.dart
@@ -19,51 +19,228 @@ class _ProductsScreenState extends State<ProductsScreen> {
   var products = [
     {
       "id": 1,
-      "name": "MacBook Pro 2024 M2 Pro",
+      "name": "Kahvaltı Tabagi",
+      "price": 250.00,
       "in-stock": true,
-      "price": 65000.00,
-      "photo":
-          "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/refurb-mbp14-m2-silver-202303?wid=2000&hei=1536&fmt=jpeg&qlt=95&.v=1680103614090",
+      "photo": "https://i.ibb.co/kBv5yG2/kahvalt-taba.png"
     },
     {
       "id": 2,
-      "name": "iPhone 15 Pro Max",
-      "in-stock": false,
-      "price": 0,
-      "photo":
-          "https://st-troy.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/mu793tua-apple-iphone-15-pro-max-256gb-natural-titanium-638305576694571609.jpg",
+      "name": "Menemen",
+      "menucontent": "Keçi Peynirli, Cevizli, Otlu / Beyaz Peynirli / Sucuklu",
+      "price": 130.00,
+      "in-stock": true,
+      "photo": "https://i.ibb.co/dDNWSNn/menemen-taba.png"
     },
     {
       "id": 3,
-      "name": "Asus VivoBook",
+      "name": "Çirpilmis Yumurta",
+      "price": 110.00,
       "in-stock": true,
-      "price": 35000.00,
-      "photo":
-          "https://dlcdnwebimgs.asus.com/gain/a1e0bd3b-16cf-4f88-be0d-91a90e03ab0f/",
+      "photo": "https://i.ibb.co/zfGsGgQ/rp-lm-yumurta.png"
     },
     {
       "id": 4,
-      "name": "Xiami X",
+      "name": "Hindi Fümeli Sandviç",
+      "price": 135.00,
       "in-stock": true,
-      "price": 15999.00,
-      "photo":
-          "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/refurb-mbp14-m2-silver-202303?wid=2000&hei=1536&fmt=jpeg&qlt=95&.v=1680103614090",
+      "photo": "https://i.ibb.co/jW263Tx/hindi-f-meli-sandvi.png"
     },
     {
       "id": 5,
-      "name": "Samsung S23",
-      "in-stock": false,
-      "price": 0,
-      "photo":
-          "https://st-troy.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/mu793tua-apple-iphone-15-pro-max-256gb-natural-titanium-638305576694571609.jpg",
+      "name": "Günün Çorbasi",
+      "price": 65.00,
+      "in-stock": true,
+      "photo": "https://i.ibb.co/k46nCx9/g-n-n-orbas.png"
     },
     {
       "id": 6,
-      "name": "Lenovo X1",
+      "name": "Dolu Dolu Tabak",
+      "price": 130.00,
       "in-stock": true,
-      "price": 43000.00,
-      "photo":
-          "https://dlcdnwebimgs.asus.com/gain/a1e0bd3b-16cf-4f88-be0d-91a90e03ab0f/",
+      "photo": "https://i.ibb.co/QmgY8m6/dolu-dolu-tabak.png"
+    },
+    {
+      "id": 7,
+      "name": "Pideli Yogurtlu Mini Köfte",
+      "price": 100.00,
+      "in-stock": true,
+      "photo": "https://i.ibb.co/Hnv1K44/mini-k-fte.png"
+    },
+    {
+      "id": 8,
+      "name": "Mini Çibörek",
+      "price": 95.00,
+      "in-stock": true,
+      "photo": "https://i.ibb.co/Bnd9BKH/mini-ib-rek.png"
+    },
+    {
+      "id": 9,
+      "name": "Cheeseburger",
+      "price": 245.00,
+      "in-stock": true,
+      "photo": "https://i.ibb.co/1RW5jcm/burger.png"
+    },
+    {
+      "id": 10,
+      "name": "Izgara Köfte",
+      "price": 245.00,
+      "in-stock": true,
+      "photo": "https://i.ibb.co/QdCvLh4/zgarak-fte.png"
+    },
+    {
+      "id": 11,
+      "name": "Tavuklu Mantarli Alfredo",
+      "price": 190.00,
+      "in-stock": true,
+      "photo": "https://i.ibb.co/648X5tJ/fettucinoalfredo.png"
+    },
+    {
+      "id": 12,
+      "name": "Margerita Pizza",
+      "price": 190.00,
+      "in-stock": true,
+      "photo": "https://i.ibb.co/JR6MJFN/margerita-pizza.png"
+    },
+    {
+      "id": 13,
+      "name": "Zencefil Soslu Tavuk Salata",
+      "price": 65.00,
+      "in-stock": true,
+      "photo": "https://i.ibb.co/0QhpNrf/zencefil-soslu-salata.png"
+    },
+    {
+      "id": 14,
+      "name": "Burrata Peynirli Salata",
+      "price": 130.00,
+      "in-stock": true,
+      "photo": "https://i.ibb.co/j4Y8j6T/burrata.png"
+    },
+    {
+      "id": 15,
+      "name": "Izgara Tavuk Salata",
+      "price": 100.00,
+      "in-stock": true,
+      "photo": "https://i.ibb.co/R4sf06P/zgara-tavuklu-salata.png"
+    },
+    {
+      "id": 16,
+      "name": "Avokadolu ve Peynirli Salata",
+      "price": 95.00,
+      "in-stock": true,
+      "photo": "https://i.ibb.co/RPjvLWb/avakadolu-salata.png"
+    },
+    {
+      "id": 17,
+      "name": "Çilekli Milföy",
+      "price": 100.00,
+      "in-stock": true,
+      "photo": "https://i.ibb.co/x3ffKv8/ilekli-milf-yt.png"
+    },
+    {
+      "id": 18,
+      "name": "Beyaz Çikolatalı Brownie",
+      "price": 110.00,
+      "in-stock": true,
+      "photo": "https://i.ibb.co/TYKcWfb/brownie.png"
+    },
+    {
+      "id": 19,
+      "name": "Tiramisu",
+      "price": 100.00,
+      "in-stock": true,
+      "photo": "https://i.ibb.co/nPhHYR3/tiramisu.png"
+    },
+    {
+      "id": 20,
+      "name": "Makaron Tabagi",
+      "price": 150.00,
+      "in-stock": true,
+      "photo": "https://i.ibb.co/sgV2TMF/makaron.png"
+    },
+    {
+      "id": 21,
+      "name": "Serpme Kahvaltı",
+      "in-stock": true,
+      "price": 240.00,
+      "photo": "https://i.ibb.co/34Y3BZw/B-g-Chefs-Serpme-Kahvalt.png",
+    },
+    {
+      "id": 22,
+      "name": "Menemen",
+      "in-stock": true,
+      "price": 115.00,
+      "photo": "https://i.ibb.co/YpYLDbJ/menemen.png",
+    },
+    {
+      "id": 23,
+      "name": "Sahanda Yumurta",
+      "in-stock": true,
+      "price": 90.00,
+      "photo": "https://i.ibb.co/x6VhKC5/B-g-Chefs-Sahanda-Yumurta.png",
+    },
+    {
+      "id": 24,
+      "name": "Pancake",
+      "in-stock": true,
+      "price": 150.00,
+      "photo": "https://i.ibb.co/M63Fh0d/B-g-Chefs-Pancake.png",
+    },
+    {
+      "id": 25,
+      "name": "Asure Çorbası",
+      "in-stock": true,
+      "price": 70.00,
+      "photo": "https://i.ibb.co/Pw0xCWh/B-g-Chefs-A-ure-orbas.png",
+    },
+    {
+      "id": 26,
+      "name": "Çıtır Mantı",
+      "in-stock": true,
+      "price": 115.00,
+      "photo": "https://i.ibb.co/brBb7t0/B-g-Chefs-t-r-Mant.png",
+    },
+    {
+      "id": 27,
+      "name": "Patates Tava",
+      "in-stock": true,
+      "price": 85.00,
+      "photo": "https://i.ibb.co/MNd0K5v/B-g-Chefs-Patates-Tava.png",
+    },
+    {
+      "id": 28,
+      "name": "Peri Soslu Patlican ve Humus",
+      "in-stock": true,
+      "price": 120.00,
+      "photo": "https://i.ibb.co/jWPbxG6/B-g-Chefs-Humus.png",
+    },
+    {
+      "id": 29,
+      "name": "Big Burger",
+      "in-stock": true,
+      "price": 270.00,
+      "photo": "https://i.ibb.co/wdkB7cY/B-g-Chefs-Big-Burger.png",
+    },
+    {
+      "id": 30,
+      "name": "Fettuccine Alfredo",
+      "in-stock": true,
+      "price": 155.00,
+      "photo": "https://i.ibb.co/hR014gQ/B-g-Chefs-Big-Fetticino-Alfredo.png",
+    },
+    {
+      "id": 31,
+      "name": "Izgara Bonfile Dilimleri",
+      "in-stock": true,
+      "price": 385.00,
+      "photo": "https://i.ibb.co/WHTT2JC/B-g-Chefs-Izgara-Bonfile.png",
+    },
+    {
+      "id": 32,
+      "name": "Margarita Pizza",
+      "in-stock": true,
+      "price": 220.00,
+      "photo": "https://i.ibb.co/61WdNq2/B-g-Chefs-Margarita.png",
     },
   ];
 
